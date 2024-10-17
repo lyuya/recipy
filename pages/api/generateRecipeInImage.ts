577c64aaad6cedd7bdd18cae6ff4513cb3dcc328
Yanan LYU <arol666@hotmail.com>
2024-10-17 15:27:08 +0200

    fix: remove console.log

--- pages/api/generateRecipeInImage.ts
+++ pages/api/generateRecipeInImage.ts
@@ -6,7 +6,6 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<{ data: Image | undefined} | { error: string }>,
 ) {
-    console.log('request: ', req);
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
