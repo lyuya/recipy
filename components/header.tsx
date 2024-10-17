b13004baa407f7050378886ef2a07d4ac4f461e9
Yanan LYU <arol666@hotmail.com>
2024-10-17 14:49:10 +0200

    refactor: use back <img/>

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Image from 'next/image'
 
 export default function Header() {
     const [navBg, setNavBg] = useState(false);
@@ -16,7 +15,7 @@ export default function Header() {
     return (
         <header className={"sticky top-0 z-10 " + (navBg ? "bg-white" : "bg-header")}>
             <div className="flex w-ful justify-center py-2">
-                <Image width={80} src="asset/face-1-svgrepo-com.svg" alt="bowl" />
+                <img width="80px" src="asset/face-1-svgrepo-com.svg" alt="bowl" />
                 <div className="flex items-center">
                     <h1 className="text-3xl font-serif font-extrabold text-black">Recipy</h1>
                 </div>
--- components/mainPage.tsx
+++ components/mainPage.tsx
@@ -3,7 +3,6 @@ import Typewriter from "./typewriter";
 import { PromptQuery } from "@/types/promptQuery";
 import SearchBar from './searchBar';
 import { Image as OpenAIImage } from 'openai/resources/images.mjs';
-import Image from 'next/image';
 
 export default function MainPage() {
     const [recipe, setRecipe] = useState<string>('');
@@ -55,7 +54,7 @@ export default function MainPage() {
                     <section className="h-full flex justify-center">
                         <div className="p-6 w-4/5 overflow-auto leading-5 bg-white/50">
                             {loading && (<div className="typing-loader"></div>)}
-                            {image && image.url && <div className="relative"><Image alt="dish-image" src={image.url} /></div>}
+                            {image && image.url && <div className="relative"><img src={image.url} /></div>}
                             {recipe.length > 0 && (<Typewriter text={recipe}></Typewriter>)}
                             {!(loading || recipe.length > 0 || image) && (<div>Tell me what ingredients do you want to cook, what kind of dish do you prefer, which ingredient do you want to avoid, etc.</div>)}
                         </div>
